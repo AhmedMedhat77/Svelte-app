b5dd0eedebac496a7a32bd7b63624ba429766a3b
AhmedMedhat77 <Ahmed.medhat1407@gmail.com>
2023-08-29 10:39:25 +0300

    add logic to store
    Study svelte store you can have mutable store to divide each component logic

--- src/lib/Modal/Modal.svelte
+++ src/lib/Modal/Modal.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { createEventDispatcher } from 'svelte';
-
 	const dispatch = createEventDispatcher();
 	const close = () => {
 		dispatch('close');
@@ -8,29 +7,38 @@
 	const onSubmit = () => {
 		dispatch('submit');
 	};
+	export let show;
 </script>
 
-<div class="relative z-10" aria-labelledby="modal-tite" role="dialog" aria-modal="true">
-	<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-	<div class="fixed z-10 inset-0 overflow-y-auto">
-		<div class="flex items-center sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+{#if show}
+	<div class="relative z-10" aria-labelledby="modal-tite" role="dialog" aria-modal="true">
+		<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+		<div class="fixed z-10 inset-0 overflow-y-auto">
 			<div
-				class="relative bg-white rounded-lg px-4 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full"
+				class="flex items-center sm:items-center justify-center min-h-full p-4 text-center sm:p-0"
 			>
-				<div class="mt-5 sm:mt-6">
-					<!-- Some HTML like props.children  -->
-					<form on:submit={onSubmit} preventDefault={onSubmit}>
-						<slot />
+				<div
+					class="relative bg-white rounded-lg px-4 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full"
+				>
+					<div class="mt-5 sm:mt-6">
+						<!-- Some HTML like props.children  -->
+						<form on:submit={onSubmit} preventDefault={onSubmit}>
+							<slot />
 
-						<div class="flex justify-between">
-							<button class="px-2 py-1 bg-white border rounded hover:bg-gray-200" on:click={close}>
-								Close
-							</button>
-							<slot name="right-button" />
-						</div>
-					</form>
+							<div class="flex justify-between">
+								<button
+									type="button"
+									class="px-2 py-1 bg-white border rounded hover:bg-gray-200"
+									on:click={close}
+								>
+									Close
+								</button>
+								<slot name="right-button" />
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+{/if}
--- src/lib/NewUser/NewUser.svelte
+++ src/lib/NewUser/NewUser.svelte
@@ -4,10 +4,13 @@
 	let showModal = false;
 	let newUser = {};
 	const dispatch = createEventDispatcher();
-
+	// Handle Submit
 	const handleForm = (e) => {
 		dispatch('newUser', newUser);
+		showModal = false;
 	};
+
+	$: disableBtn = !newUser.name || !newUser.userEmail;
 </script>
 
 <div class="mt-4">
@@ -17,61 +20,62 @@
 	>
 		New user
 	</button>
-	{#if showModal}
-		<Modal on:submit={handleForm} on:close={() => (showModal = false)}>
-			<!-- this is the slot we cant add slot inside slot content -->
-			<h1 class="text-2xl text-center">Create New User</h1>
 
-			<div class="py-1 px-2 my-4">
-				<label for="userName"> Name </label>
+	<Modal show={showModal} on:submit={handleForm} on:close={() => (showModal = false)}>
+		<!-- this is the slot we cant add slot inside slot content -->
+		<h1 class="text-2xl text-center">Create New User</h1>
+
+		<div class="py-1 px-2 my-4">
+			<label for="userName"> Name </label>
+			<input
+				class="px-2 py-1 rounded border w-full"
+				bind:value={newUser.userName}
+				type="text"
+				name="userName"
+				id="userName"
+			/>
+		</div>
+
+		<div class="py-1 px-2 my-4">
+			<label for="userEmail"> Email </label>
+			<input
+				class="px-2 py-1 rounded border w-full"
+				type="userEmail"
+				name="userEmail"
+				id="userEmail"
+				bind:value={newUser.userEmail}
+			/>
+		</div>
+		<div class="py-1 px-2 my-4 flex justify-between">
+			<label for="true"> Active: </label>
+			<div class="flex">
+				<label for="true"> yes </label>
 				<input
-					class="px-2 py-1 rounded border w-full"
-					bind:value={newUser.userName}
-					type="text"
-					name="userName"
-					id="userName"
+					class="px-2 py-1 w-full rounded border mx-5"
+					type="radio"
+					value="true"
+					name="active"
+					id="true"
+					bind:group={newUser.active}
+				/>
+				<label for="false"> no </label>
+				<input
+					class="px-2 py-1 w-full rounded border mx-5"
+					type="radio"
+					value="false"
+					name="active"
+					id="false"
+					bind:group={newUser.active}
 				/>
 			</div>
+		</div>
 
-			<div class="py-1 px-2 my-4">
-				<label for="userEmail"> Email </label>
-				<input
-					class="px-2 py-1 rounded border w-full"
-					type="userEmail"
-					name="userEmail"
-					id="userEmail"
-					bind:value={newUser.userEmail}
-				/>
-			</div>
-			<div class="py-1 px-2 my-4 flex justify-between">
-				<label for="true"> Active: </label>
-				<div class="flex">
-					<label for="true"> yes </label>
-					<input
-						class="px-2 py-1 w-full rounded border mx-5"
-						type="radio"
-						value="true"
-						name="active"
-						id="true"
-						bind:group={newUser.active}
-					/>
-					<label for="false"> no </label>
-					<input
-						class="px-2 py-1 w-full rounded border mx-5"
-						type="radio"
-						value="false"
-						name="active"
-						id="false"
-						bind:group={newUser.active}
-					/>
-				</div>
-			</div>
-
-			<button
-				type="submit"
-				slot="right-button"
-				class="px-2 py-1 bg-blue-400 text-white rounded hover:bg-blue-700">Create</button
-			>
-		</Modal>
-	{/if}
+		<button
+			disabled={false}
+			id="submitBtn"
+			type="submit"
+			slot="right-button"
+			class="px-2 py-1 bg-blue-400 text-white rounded hover:bg-blue-700">Create</button
+		>
+	</Modal>
 </div>
